a00496358377cfcc006fb72c6ec36c102dec2a81
jdsavaj <58255824+jdsavaj@users.noreply.github.com>
2019-12-06 16:35:18 +0530

    Update index.php

--- backend-queue-job-php/index.php
+++ backend-queue-job-php/index.php
@@ -5,21 +5,24 @@
   <title></title>
 </head>
 <body>
+  <input type="text" name="altname" id="altname">
   <button id="btnSubmit">Submit</button>
 </body>
 </html>
 <script type="text/javascript">
 $(document).ready(function(){
-	$(document).on('click','#btnSubmit',function(){
-      	window.location.href = "http://localhost/z/backend-queue-job-php/demo.php";
+	$(document).on('click','#btnSubmit',function(){ 
+	var altname = $("#altname").val();
       	$.ajax({
 	        url:"http://localhost/z/backend-queue-job-php/queue_job.php",
 	        type:"POST",
+	        data:{altname:altname},
 	        success:function()
 	        {	
 		        
-		    }
+		}
       	});
+      	window.location.href = "http://localhost/z/backend-queue-job-php/demo.php";
   	});
 });	
 </script>
